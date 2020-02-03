f6462afdacd091d92f670a3e7dce3d37143cbc1b
Spawn <alkspawn@gmail.com>
2020-02-03 09:15:34 +0300

    [server] create user and role fix pom

---
+++ server/src/main/java/com/it/alex/server/SamStarter.java
@@ -0,0 +1,11 @@
+package com.it.alex.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(SamStarter.class,args);
+    }
+}
---
+++ server/src/main/java/com/it/alex/server/model/RoleName.java
@@ -0,0 +1,6 @@
+package com.it.alex.server.model;
+
+public enum  RoleName {
+    ROLE_USER,
+    ROLE_ADMIN
+}
